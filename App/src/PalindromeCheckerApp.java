f217019f2050b6ad892d05164510d363486291ae
srishtiiii19 <srishtisri1935@gmail.com>
2026-02-27 16:31:45 +0530

    [Srishti] added [UseCase2]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully");
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
