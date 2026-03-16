a597df321d46d661de94a8c17739b38ec2dfa2f8
srishtiiii19 <srishtisri1935@gmail.com>
2026-03-16 23:43:23 +0530

    Srishti Added UC13

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,34 +1,80 @@
-class PalindromeChecker {
+import java.util.*;
 
-    // Method to check palindrome
-    public boolean checkPalindrome(String input) {
+public class PalindromeCheckerApp {
 
-        char[] arr = input.toCharArray();
-        int start = 0;
-        int end = arr.length - 1;
+    // Stack approach
+    static boolean stackPalindrome(String input) {
+        Stack<Character> stack = new Stack<>();
 
-        while (start < end) {
-            if (arr[start] != arr[end]) {
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
 
         return true;
     }
-}
 
-public class PalindromeCheckerApp {
+    // Deque approach
+    static boolean dequePalindrome(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Recursion approach
+    static boolean recursionPalindrome(String str, int start, int end) {
+        if (start >= end)
+            return true;
+
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return recursionPalindrome(str, start + 1, end - 1);
+    }
 
     public static void main(String[] args) {
 
-        String input = "racecar";
+        String input = "level";
 
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean result = checker.checkPalindrome(input);
+        // Stack timing
+        long start = System.nanoTime();
+        boolean stackResult = stackPalindrome(input);
+        long stackTime = System.nanoTime() - start;
 
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome? : " + result);
+        // Deque timing
+        start = System.nanoTime();
+        boolean dequeResult = dequePalindrome(input);
+        long dequeTime = System.nanoTime() - start;
+
+        // Recursion timing
+        start = System.nanoTime();
+        boolean recursionResult = recursionPalindrome(input, 0, input.length() - 1);
+        long recursionTime = System.nanoTime() - start;
+
+        // Final result (all algorithms give same result)
+        boolean finalResult = stackResult;
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + finalResult);
+        System.out.println();
+
+        System.out.println("Stack Time : " + stackTime + " ns");
+        System.out.println("Deque Time : " + dequeTime + " ns");
+        System.out.println("Recursion Time : " + recursionTime + " ns");
     }
 }
